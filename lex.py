60d80a8439ff199b347c1f8e8ec5a6e03394b4d2
mouhcine03 <mouhcine.elhasnaoui03@gmail.com>
2024-12-15 16:23:48 +0100

    conflit resolution final

--- lex.py
+++ lex.py
@@ -526,17 +526,11 @@ labelRSEM.place(x=50,y=460)
 window.mainloop()
 
 
-# Main function
+#Main function
 def main():
-    tokens_list = lexicalAnalysis()  # Perform lexical analysis
-    if tokens_list:
-        print("\n-----------------------------Syntactic analysis-----------------------------")
-        process_lines(tokens_list)  # Process the tokens line by line
-    else:
-        print("Error in lexical analysis. Syntactic analysis will not be performed.")
     
     
 
-# Entry point
-if __name__ == "__main__":
+ #Entry point 
+ if __name__ == "__main__":
     main()
